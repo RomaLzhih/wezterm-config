87ba6d5c475a9c96dd7d3138dadd94d6e9559a15
SwansZiyangswan <ziyang@SwansSwan.local>
2025-04-05 14:32:51 +0800

    update mac default terminal

--- config/launch.lua
+++ config/launch.lua
@@ -8,24 +8,25 @@ local options = {
 if platform.is_win then
    options.default_prog = { 'powershell' }
    options.launch_menu = {
-      { label = 'PowerShell Core', args = { 'pwsh' } },
+      { label = 'PowerShell Core',    args = { 'pwsh' } },
       { label = 'PowerShell Desktop', args = { 'powershell' } },
-      { label = 'Command Prompt', args = { 'cmd' } },
+      { label = 'Command Prompt',     args = { 'cmd' } },
    }
 elseif platform.is_mac then
-   options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+   -- options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+   options.default_prog = { 'zsh', '-l' }
    options.launch_menu = {
-      { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { '/opt/homebrew/bin/fish', '-l' } },
+      { label = 'Bash',    args = { 'bash', '-l' } },
+      { label = 'Fish',    args = { '/opt/homebrew/bin/fish', '-l' } },
       { label = 'Nushell', args = { '/opt/homebrew/bin/nu', '-l' } },
-      { label = 'Zsh', args = { 'zsh', '-l' } },
+      { label = 'Zsh',     args = { 'zsh', '-l' } },
    }
 elseif platform.is_linux then
    options.default_prog = { 'zsh', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
       { label = 'Fish', args = { 'fish', '-l' } },
-      { label = 'Zsh', args = { 'zsh', '-l' } },
+      { label = 'Zsh',  args = { 'zsh', '-l' } },
    }
 end
 
